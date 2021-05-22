6ec61c4a141a2d28da97e29e7b8ee8127400a52c
Gleb Chistyakov <gleb4ist@gmail.com>
2021-05-22 14:07:31 +0300

    btn comp without icons

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,11 +1,62 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button as BtnAntd } from 'antd';
+import classNames from 'classnames';
 
-import './Button.scss'
-
-const Button: React.FC = () => {
-  return (
-    <button>123</button>
-  );
+export interface IColorScheme {
+  colorScheme?: 'green' | 'outline';
 }
+
+export interface ISize {
+  size?: 'sm' | 'smd' | 'md' | 'lmd' | 'lg';
+}
+
+export interface ButtonProps extends IColorScheme, ISize {
+  onClick?: () => void;
+  className?: string;
+  disabled?: boolean;
+  loading?: boolean;
+  link?: string;
+  linkClassName?: string;
+  shadow?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  className,
+  size = 'md',
+  colorScheme = 'green',
+  onClick,
+  disabled = false,
+  loading = false,
+  link,
+  linkClassName,
+}) => {
+  const Btn = (
+    <BtnAntd
+      onClick={onClick}
+      disabled={disabled || loading}
+      className={classNames(
+        className || '',
+        'text text-bold btn',
+        `btn-${size}`,
+        `btn-${colorScheme}`,
+        {
+          'btn-loading': loading,
+        },
+      )}
+    >
+      {loading ? 'In progress...' : children}
+    </BtnAntd>
+  );
+  if (link) {
+    return (
+      <Link className={classNames('btn-link', linkClassName)} to={link}>
+        {Btn}
+      </Link>
+    );
+  }
+  return Btn;
+};
 
 export default Button;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 import './Home.scss';
 
+import { Button } from '../../components/atoms';
+
 const Home: React.FC = () => {
   return (
     <div className="home">
-      <main>home1</main>
+      <main>
+        <Button>
+          <div className="text-upper text-bold text-slg">Stake</div>
+        </Button>
+      </main>
     </div>
   );
 };
