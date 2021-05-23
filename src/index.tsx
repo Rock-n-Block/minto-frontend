9e90c2fdc9174826cbfe136638c7e6bf3e3fe73b
Gleb Chistyakov <gleb4ist@gmail.com>
2021-05-23 14:20:20 +0300

    scroll to top after change page

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import { App } from './App';
+import ScollToTop from './utils/ScollToTop';
 
 ReactDOM.render(
   <Router>
-    <App />
+    <ScollToTop>
+      <App />
+    </ScollToTop>
   </Router>,
   document.getElementById('root'),
 );
