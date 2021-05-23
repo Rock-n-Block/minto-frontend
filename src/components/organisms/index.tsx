00cd5c413f025ea7acc3d15aefeb0c9d479884c3
Gleb Chistyakov <gleb4ist@gmail.com>
2021-05-23 13:53:58 +0300

    procedure component without adaptive

--- src/components/organisms/index.tsx
+++ src/components/organisms/index.tsx
@@ -2,3 +2,4 @@ export { default as Header } from './Header';
 export { default as Footer } from './Footer';
 export { default as Subscribe } from './Subscribe';
 export { default as About } from './About';
+export { default as Procedure } from './Procedure';
