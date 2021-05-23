bd0dd87500c047a66df958c26cf620332be2df20
Gleb Chistyakov <gleb4ist@gmail.com>
2021-05-23 14:25:23 +0300

    remove subs from about page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import 'swiper/swiper.scss';
 
+import { About, Subscribe } from '../../components/organisms';
+import { AboutContacts, AboutTeam } from '../../components/sections';
 
-import {About, Subscribe} from "../../components/organisms";
-import {AboutContacts, AboutTeam} from "../../components/sections";
-
-const AboutPage: React.FC = ( ) => {
+const AboutPage: React.FC = () => {
   return (
     <main className="about">
       <About />
-      <AboutContacts/>
-      <AboutTeam/>
-      <Subscribe/>
+      <AboutContacts />
+      <AboutTeam />
     </main>
   );
 };
