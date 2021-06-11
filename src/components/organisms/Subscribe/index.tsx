64abf385d9648b97bbb3df94e94b340a68148fb1
Amary Filo <laumiqv@gmail.com>
2021-06-11 15:37:29 +0300

    upd: dev area

--- src/components/organisms/Subscribe/index.tsx
+++ src/components/organisms/Subscribe/index.tsx
@@ -1,14 +1,47 @@
 import React from 'react';
 
 import IconMail from '../../../assets/img/icons/mail-package.svg';
+import { API, clogData, notify } from '../../../utils';
 import { Button, Input } from '../../atoms';
 
 import './Subscribe.scss';
 
 const Subscribe: React.FC = () => {
+  const [email, setEmail] = React.useState('');
+  const [isProcess, setIsProcess] = React.useState(false);
+
+  const handleSubscribeClick = async () => {
+    setIsProcess(true);
+
+    API.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+    API.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
+    await API.post('/email/subscribe/', {
+      email,
+    })
+      .then((res: any) => {
+        clogData('subscribe: ', res);
+        notify('Thanks for subscribing!', 'success');
+        setEmail('');
+      })
+      .catch((err: any) => {
+        clogData('error in subscribe', err);
+        notify('Something went wrong! Please try again later.', 'error');
+      })
+      .finally(() => {
+        setIsProcess(false);
+      });
+  };
+
   return (
     <div className="subscribe">
-      <div className="row">
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubscribeClick();
+        }}
+      >
         <div className="subscribe__icon circle">
           <img src={IconMail} alt="mail" />
         </div>
@@ -16,12 +49,27 @@ const Subscribe: React.FC = () => {
           We’re launching soon, subscribe to be the first to know!
         </h2>
         <div className="subscribe__e-mail input-container">
-          <Input placeholder="Enter your email" colorScheme="white" size="md" shadow />
+          <Input
+            placeholder="Enter your email"
+            type="email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+            colorScheme="white"
+            value={email}
+            size="md"
+            shadow
+          />
         </div>
-        <Button size="smd" colorScheme="green">
-          <span className="text-upper text-bold-e text-smd">Subscribe</span>
-        </Button>
-      </div>
+        {isProcess ? (
+          <Button size="smd" disabled type="submit" colorScheme="green">
+            <span className="text-upper text-bold-e text-smd">Processing...</span>
+          </Button>
+        ) : (
+          <Button size="smd" type="submit" colorScheme="green">
+            <span className="text-upper text-bold-e text-smd">Subscribe</span>
+          </Button>
+        )}
+      </form>
     </div>
   );
 };
