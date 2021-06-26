a855b35327e1be00d8f0a854a9c18e0c8ba1505d
Amary Filo <laumiqv@gmail.com>
2021-06-26 03:38:21 +0300

    add line chart

--- src/pages/Statistic/index.tsx
+++ src/pages/Statistic/index.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback } from 'react';
+import { Line } from 'react-chartjs-2';
 import { useTranslation } from 'react-i18next';
 import nextId from 'react-id-generator';
 import cn from 'classnames';
@@ -54,6 +55,41 @@ const Statistic: React.FC = () => {
   } as IData);
 
   const { t } = useTranslation();
+
+  const dataChart = {
+    labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'],
+    datasets: [
+      {
+        label: 'BTCMT',
+        tension: 0.3,
+        data: [12, 19, 3, 5, 2, 3, 123, 77, 44, 66, 54],
+        fill: true,
+        backgroundColor: 'rgba(109, 218, 192,0.1)',
+        borderColor: 'rgb(109, 218, 192)',
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      // xAxes: {
+      //   display: false,
+      // },
+      yAxes: [
+        {
+          ticks: {
+            display: false,
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  };
 
   const getInfo = useCallback(async () => {
     const { address } = store.account;
@@ -181,16 +217,6 @@ const Statistic: React.FC = () => {
 
   // On Run ------------------------------------------------
 
-  // TODO: FIX requests
-  // React.useEffect(() => {
-  //   if (!store.account.address && config.menu.onlyForAuth) {
-  //     store.toggleWalletMenu(true);
-  //     return;
-  //   }
-  //   if (!store.account.address) return;
-  //   getInfo();
-  // }, [getInfo, store]);
-
   React.useEffect(() => {
     getInfo();
   }, [getInfo, store.account.address, store]);
@@ -225,6 +251,9 @@ const Statistic: React.FC = () => {
                 </span>
               ))}
             </div>
+          </div>
+          <div className="stats-data-chart-line">
+            <Line type="line" data={dataChart} options={options} />
           </div>
         </div>
         <div className="stats-data-info">
@@ -285,231 +314,8 @@ const Statistic: React.FC = () => {
           value: `${0}`,
         }}
       />
-
-      {/* {store.account.address ? (
-        <div>
-          <HistoryTable
-            title="HBTC reward history (total)"
-            head={{
-              date: `${t('page.mining.history.table.col.0')}`,
-              revard: `${t('page.mining.history.table.col.1')}`,
-            }}
-            body={tdata.history}
-            total={{
-              title: 'Total',
-              value: `${0}`,
-            }}
-          />
-        </div>
-      ) : (
-        <div className="no_login_data">
-          <span className="links__title text-center text text-black text-bold-e">
-            {t('info.connectWallet')}
-          </span>
-        </div>
-      )} */}
     </div>
   );
 };
 
 export default observer(Statistic);
-
-// import React, { useCallback } from 'react';
-// import { useTranslation } from 'react-i18next';
-// import { NavLink } from 'react-router-dom';
-// import { observer } from 'mobx-react-lite';
-// import Web3 from 'web3';
-
-// import { Button, Info } from '../../components/atoms';
-// import { config, contracts } from '../../config';
-// import { useStore } from '../../store';
-// import { IData } from '../../types';
-// import { clogData, dataToObject, getDailyRewards, normalizedValue } from '../../utils';
-
-// import './Statistic.scss';
-
-// const Statistic: React.FC = () => {
-//   const store = useStore();
-//   const { t } = useTranslation();
-
-//   const [info, setInfo] = React.useState({
-//     available: '-',
-//     totalSupply: '-',
-//     totalHashrate: '-',
-//     dailyRewards: '-',
-//     alreadyStaked: '-',
-//     allTimeMined: '-',
-//   } as IData);
-
-//   const getInfo = useCallback(async () => {
-//     const { address } = store.account;
-
-//     address ? (!store.is_contractService ? store.setContractService() : null) : null;
-//     const web3Contract = address ? store.contracts : ([] as any);
-
-//     if (!address) {
-//       const w3 = new Web3(config.provider);
-//       contracts.names.forEach((name: string) => {
-//         const contractData = contracts.params[name.toUpperCase()][contracts.type];
-//         web3Contract[name] = new w3.eth.Contract(contractData.abi, contractData.address);
-//       });
-//     }
-
-//     const promises = [
-//       // TODO: посчитать - узнать откуда брать данные
-//       web3Contract.Token.methods
-//         .totalSupply()
-//         .call()
-//         .then((value: string) => {
-//           clogData('totalStaked (totalStaked): ', value);
-//           return {
-//             key: 'totalStaked',
-//             value: normalizedValue(value),
-//           };
-//         }),
-//       // TODO: посчитать - узнать откуда брать данные
-//       web3Contract.Token.methods
-//         .totalSupply()
-//         .call()
-//         .then((value: string) => {
-//           clogData('totalIssued (totalIssued): ', value);
-//           return {
-//             key: 'totalIssued',
-//             value: normalizedValue(value),
-//           };
-//         }),
-//       web3Contract.Staking.methods
-//         .allTimeTotalMined()
-//         .call()
-//         .then((value: string) => {
-//           clogData('allTimeMined (allTimeMined): ', value);
-//           return {
-//             key: 'allTimeMined',
-//             value: normalizedValue(value),
-//           };
-//         }),
-//       // TODO: посчитать - узнать откуда брать данные
-//       web3Contract.Token.methods
-//         .totalSupply()
-//         .call()
-//         .then((value: string) => {
-//           clogData('boostFactor (boostFactor): ', value);
-//           return {
-//             key: 'boostFactor',
-//             value: normalizedValue(value),
-//           };
-//         }),
-//       new Promise((resolve) => {
-//         clogData('estimateDailyRewardsToday (estimateDailyRewardsToday): ', 50_000);
-//         resolve({
-//           key: 'estimateDailyRewardsToday',
-//           value: '50 000',
-//         });
-//       }),
-//       // TODO: посчитать - узнать правильно ли получаю данные
-//       await getDailyRewards()
-//         .then((value: number) => {
-//           clogData('rewardPerTokenWithBoost (rewardPerTokenWithBoost): ', value);
-//           return {
-//             key: 'rewardPerTokenWithBoost',
-//             value: normalizedValue(value),
-//           };
-//         })
-//         .catch((err: any) => clogData('rewardPerTokenWithBoost error:', err)),
-//       // TODO: посчитать - узнать откуда брать данные
-//       await getDailyRewards()
-//         .then((value: number) => {
-//           clogData('rewardsPerTokenWithBoost (rewardsPerTokenWithBoost): ', value);
-//           return {
-//             key: 'rewardPerTokenWithBoost',
-//             value: normalizedValue(value),
-//           };
-//         })
-//         .catch((err: any) => clogData('rewardsPerTokenWithBoost error:', err)),
-//     ];
-
-//     const uinfo = await Promise.all(promises).then((results): IData => {
-//       return dataToObject(results, true, 'Stats page normilized values');
-//     });
-
-//     // await API.get('/total/history/')
-//     //   .then((res: any) => {
-//     //     clogData('Total history: ', res);
-//     //     settData(res.data);
-//     //   })
-//     //   .catch((error: any) => {
-//     //     if (error.response)
-//     //       clogData(`Total got error status ${error.response.status}: `, error.response.data);
-//     //   });
-
-//     setInfo(uinfo);
-//   }, [store]);
-
-//   // On Run ------------------------------------------------
-
-//   React.useEffect(() => {
-//     getInfo();
-//   }, [getInfo, store.account.address, store]);
-
-//   // Template ------------------------------------------------
-
-//   return (
-//     <div className="home__preview">
-//       <div className="row">
-//         <h1 className="h1 text-bold home__preview-title">{t('page.home.title')}</h1>
-//         <div className="text-lg home__preview-subtitle">{t('page.home.subtitle')}</div>
-//         <div className="box-f box-f-ai-c home__preview-box">
-//           <Button size="lmd" className="home__preview-btn">
-//             <NavLink exact to="/staking" className="text-upper text-slg">
-//               {t('page.home.buttons.stake')}
-//             </NavLink>
-//           </Button>
-//           <Button size="md" colorScheme="outline" className="home__preview-btn">
-//             <div className="text-upper text-slg">{t('page.home.buttons.buy')}</div>
-//           </Button>
-//         </div>
-//         <div className="home__preview-info box-f box-f-ai-c">
-//           <Info
-//             content="$42"
-//             topText={t('page.home.tokenPrice')}
-//             className="home__preview-info-item"
-//           />
-//           <Info
-//             content={info.totalSupply}
-//             topText={t('page.home.totalSupply')}
-//             bottomText="BTCMT"
-//             className="home__preview-info-item"
-//           />
-//           <Info
-//             content={info.totalHashrate}
-//             topText="Total hashrate"
-//             bottomText="TH/s"
-//             className="home__preview-info-item"
-//           />
-//         </div>
-//         <div className="home__preview-info box-f box-f-ai-c">
-//           <Info
-//             content={info.dailyRewards}
-//             topText="Daily rewards"
-//             bottomText="USD"
-//             className="home__preview-info-item"
-//           />
-//           <Info
-//             content={info.alreadyStaked}
-//             topText="Already staked"
-//             bottomText="BTCMT"
-//             className="home__preview-info-item"
-//           />
-//           <Info
-//             content={info.allTimeMined}
-//             topText="All time mined"
-//             bottomText="HBTC"
-//             className="home__preview-info-item"
-//           />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default observer(Statistic);
