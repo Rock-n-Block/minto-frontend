2fde58a1659a5d65830862b560ae8ad9c0941ca9
Kirill Kalinichenko <ka.ki.ko.1995@gmail.com>
2021-07-01 19:16:11 +0300

    fixed lint warning

--- src/components/organisms/index.tsx
+++ src/components/organisms/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-cycle */
 export { default as Header } from './Header';
 export { default as HeaderAdmin } from './HeaderAdmin';
 export { default as Footer } from './Footer';
