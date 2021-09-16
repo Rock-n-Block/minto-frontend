0ca8a97486eebf58ea30a2bd0f77f565f236f1dd
Amary Filo <lamuiqv@gmail.com>
2021-09-16 12:42:19 +0300

    upd statistic page (remove chart return values)

--- src/pages/Statistic/index.tsx
+++ src/pages/Statistic/index.tsx
@@ -1,37 +1,38 @@
 // export {};
 
 import React, { useCallback } from 'react';
-import { Line } from 'react-chartjs-2';
+// import { Line } from 'react-chartjs-2';
 import { useTranslation } from 'react-i18next';
-import nextId from 'react-id-generator';
+// import nextId from 'react-id-generator';
 import axios from 'axios';
-import cn from 'classnames';
+import BigNumber from 'bignumber.js/bignumber.js';
+// import cn from 'classnames';
 import { observer } from 'mobx-react-lite';
-import moment from 'moment';
+// import moment from 'moment';
 import Web3 from 'web3';
 
 import { Calculator, HistoryTable } from '../../components/organisms';
 import { chain, contracts } from '../../config';
 import { useStore } from '../../store';
 import { IData } from '../../types';
-import { API, clogData, dataToObject, getDailyRewards } from '../../utils';
+import { API, clogData, dataToObject, getDailyRewards, normalizedValue } from '../../utils';
 
 import './Statistic.scss';
 
-interface IChartData {
-  [index: string]: IChartDataItem[];
-}
-interface IChartDataItem {
-  x: string;
-  y: number;
-}
+// interface IChartData {
+//   [index: string]: IChartDataItem[];
+// }
+// interface IChartDataItem {
+//   x: string;
+//   y: number;
+// }
 
-interface IChartButton {
-  id: number;
-  title: string;
-  info: string;
-  active: boolean;
-}
+// interface IChartButton {
+//   id: number;
+//   title: string;
+//   info: string;
+//   active: boolean;
+// }
 
 const Statistic: React.FC = () => {
   const store = useStore();
@@ -40,38 +41,38 @@ const Statistic: React.FC = () => {
 
   // const [tdata, settData] = React.useState({ total: '0', history: [] } as IUserHistory);
   const [tdata, settData] = React.useState([]);
-  const [chartButton, setChartButton] = React.useState(0);
+  // const [chartButton, setChartButton] = React.useState(0);
 
   const [rewardCalcValue, setRewardCalcValue] = React.useState(0);
   const [dailyReward, setDailyReward] = React.useState(0);
   const [dailyRewardUsd, setDailyRewardUsd] = React.useState(0);
 
-  const [chartButtons, setChartButtons] = React.useState([
-    {
-      id: 0,
-      title: '1d',
-      info: 'day',
-      active: true,
-    },
-    {
-      id: 1,
-      title: '1w',
-      info: 'week',
-      active: false,
-    },
-    {
-      id: 2,
-      title: '1m',
-      info: 'month',
-      active: false,
-    },
-    {
-      id: 3,
-      title: '1y',
-      info: 'year',
-      active: false,
-    },
-  ] as IChartButton[]);
+  // const [chartButtons, setChartButtons] = React.useState([
+  //   {
+  //     id: 0,
+  //     title: '1d',
+  //     info: 'day',
+  //     active: true,
+  //   },
+  //   {
+  //     id: 1,
+  //     title: '1w',
+  //     info: 'week',
+  //     active: false,
+  //   },
+  //   {
+  //     id: 2,
+  //     title: '1m',
+  //     info: 'month',
+  //     active: false,
+  //   },
+  //   {
+  //     id: 3,
+  //     title: '1y',
+  //     info: 'year',
+  //     active: false,
+  //   },
+  // ] as IChartButton[]);
 
   const [info, setInfo] = React.useState({
     totalStaked: '-',
@@ -83,108 +84,108 @@ const Statistic: React.FC = () => {
     rewardsPerTokenWithBoost: '-',
   } as IData);
 
-  const chart = {
-    day: [
-      { x: moment.utc('2021-06-01T01:00:00.508Z').format('HH:mm'), y: 0 },
-      { x: moment.utc('2021-06-01T02:00:00.508Z').format('HH:mm'), y: 0 },
-      { x: moment.utc('2021-06-01T03:00:00.508Z').format('HH:mm'), y: 0 },
-      { x: moment.utc('2021-06-01T04:00:00.508Z').format('HH:mm'), y: 0 },
-      { x: moment.utc('2021-06-01T05:00:00.508Z').format('HH:mm'), y: 0 },
-      { x: moment.utc('2021-06-01T06:00:00.508Z').format('HH:mm'), y: 0 },
-      { x: moment.utc('2021-06-01T07:00:00.508Z').format('HH:mm'), y: 0 },
-      { x: moment.utc('2021-06-01T08:00:00.508Z').format('HH:mm'), y: 0 },
-      { x: moment.utc('2021-06-01T09:00:00.508Z').format('HH:mm'), y: 0 },
-      { x: moment.utc('2021-06-01T10:00:00.508Z').format('HH:mm'), y: 0 },
-      { x: moment.utc('2021-06-01T11:00:00.508Z').format('HH:mm'), y: 0 },
-      { x: moment.utc('2021-06-01T12:00:00.508Z').format('HH:mm'), y: 0 },
-      { x: moment.utc('2021-06-01T13:00:00.508Z').format('HH:mm'), y: 0 },
-      { x: moment.utc('2021-06-01T14:00:00.508Z').format('HH:mm'), y: 0 },
-      { x: moment.utc('2021-06-01T15:00:00.508Z').format('HH:mm'), y: 0 },
-      { x: moment.utc('2021-06-01T16:00:00.508Z').format('HH:mm'), y: 0 },
-      { x: moment.utc('2021-06-01T17:00:00.508Z').format('HH:mm'), y: 0 },
-      { x: moment.utc('2021-06-01T18:00:00.508Z').format('HH:mm'), y: 0 },
-      { x: moment.utc('2021-06-01T19:00:00.508Z').format('HH:mm'), y: 0 },
-      { x: moment.utc('2021-06-01T20:00:00.508Z').format('HH:mm'), y: 0 },
-      { x: moment.utc('2021-06-01T21:00:00.508Z').format('HH:mm'), y: 0 },
-      { x: moment.utc('2021-06-01T22:00:00.508Z').format('HH:mm'), y: 0 },
-    ],
-    week: [
-      { x: moment.utc('2021-04-26T06:00:00.508Z').format('YYYY/MM/DD'), y: 0 },
-      { x: moment.utc('2021-05-03T06:00:00.508Z').format('YYYY/MM/DD'), y: 0 },
-      { x: moment.utc('2021-05-10T06:00:00.508Z').format('YYYY/MM/DD'), y: 0 },
-      { x: moment.utc('2021-05-17T06:00:00.508Z').format('YYYY/MM/DD'), y: 0 },
-      { x: moment.utc('2021-05-24T06:00:00.508Z').format('YYYY/MM/DD'), y: 0 },
-      { x: moment.utc('2021-05-31T06:00:00.508Z').format('YYYY/MM/DD'), y: 0 },
-      { x: moment.utc('2021-06-07T06:00:00.508Z').format('YYYY/MM/DD'), y: 0 },
-    ],
-    month: [
-      { x: moment.utc('2021-01-01T00:00:00.508Z').format('YYYY/MM'), y: 0 },
-      { x: moment.utc('2021-02-01T00:00:00.508Z').format('YYYY/MM'), y: 0 },
-      { x: moment.utc('2021-03-01T00:00:00.508Z').format('YYYY/MM'), y: 0 },
-      { x: moment.utc('2021-04-01T00:00:00.508Z').format('YYYY/MM'), y: 0 },
-      { x: moment.utc('2021-05-01T00:00:00.508Z').format('YYYY/MM'), y: 0 },
-      { x: moment.utc('2021-06-01T00:00:00.508Z').format('YYYY/MM'), y: 0 },
-      { x: moment.utc('2021-07-01T00:00:00.508Z').format('YYYY/MM'), y: 0 },
-    ],
-    year: [
-      { x: moment.utc('2020-01-01T00:00:00.508Z').format('YYYY'), y: 0 },
-      { x: moment.utc('2021-01-01T00:00:00.508Z').format('YYYY'), y: 0 },
-      { x: moment.utc('2022-01-01T00:00:00.508Z').format('YYYY'), y: 0 },
-      { x: moment.utc('2023-01-01T00:00:00.508Z').format('YYYY'), y: 0 },
-      { x: moment.utc('2024-01-01T00:00:00.508Z').format('YYYY'), y: 0 },
-      { x: moment.utc('2025-01-01T00:00:00.508Z').format('YYYY'), y: 0 },
-      { x: moment.utc('2026-01-01T00:00:00.508Z').format('YYYY'), y: 0 },
-    ],
-  } as IChartData;
+  // const chart = {
+  //   day: [
+  //     { x: moment.utc('2021-06-01T01:00:00.508Z').format('HH:mm'), y: 0 },
+  //     { x: moment.utc('2021-06-01T02:00:00.508Z').format('HH:mm'), y: 0 },
+  //     { x: moment.utc('2021-06-01T03:00:00.508Z').format('HH:mm'), y: 0 },
+  //     { x: moment.utc('2021-06-01T04:00:00.508Z').format('HH:mm'), y: 0 },
+  //     { x: moment.utc('2021-06-01T05:00:00.508Z').format('HH:mm'), y: 0 },
+  //     { x: moment.utc('2021-06-01T06:00:00.508Z').format('HH:mm'), y: 0 },
+  //     { x: moment.utc('2021-06-01T07:00:00.508Z').format('HH:mm'), y: 0 },
+  //     { x: moment.utc('2021-06-01T08:00:00.508Z').format('HH:mm'), y: 0 },
+  //     { x: moment.utc('2021-06-01T09:00:00.508Z').format('HH:mm'), y: 0 },
+  //     { x: moment.utc('2021-06-01T10:00:00.508Z').format('HH:mm'), y: 0 },
+  //     { x: moment.utc('2021-06-01T11:00:00.508Z').format('HH:mm'), y: 0 },
+  //     { x: moment.utc('2021-06-01T12:00:00.508Z').format('HH:mm'), y: 0 },
+  //     { x: moment.utc('2021-06-01T13:00:00.508Z').format('HH:mm'), y: 0 },
+  //     { x: moment.utc('2021-06-01T14:00:00.508Z').format('HH:mm'), y: 0 },
+  //     { x: moment.utc('2021-06-01T15:00:00.508Z').format('HH:mm'), y: 0 },
+  //     { x: moment.utc('2021-06-01T16:00:00.508Z').format('HH:mm'), y: 0 },
+  //     { x: moment.utc('2021-06-01T17:00:00.508Z').format('HH:mm'), y: 0 },
+  //     { x: moment.utc('2021-06-01T18:00:00.508Z').format('HH:mm'), y: 0 },
+  //     { x: moment.utc('2021-06-01T19:00:00.508Z').format('HH:mm'), y: 0 },
+  //     { x: moment.utc('2021-06-01T20:00:00.508Z').format('HH:mm'), y: 0 },
+  //     { x: moment.utc('2021-06-01T21:00:00.508Z').format('HH:mm'), y: 0 },
+  //     { x: moment.utc('2021-06-01T22:00:00.508Z').format('HH:mm'), y: 0 },
+  //   ],
+  //   week: [
+  //     { x: moment.utc('2021-04-26T06:00:00.508Z').format('YYYY/MM/DD'), y: 0 },
+  //     { x: moment.utc('2021-05-03T06:00:00.508Z').format('YYYY/MM/DD'), y: 0 },
+  //     { x: moment.utc('2021-05-10T06:00:00.508Z').format('YYYY/MM/DD'), y: 0 },
+  //     { x: moment.utc('2021-05-17T06:00:00.508Z').format('YYYY/MM/DD'), y: 0 },
+  //     { x: moment.utc('2021-05-24T06:00:00.508Z').format('YYYY/MM/DD'), y: 0 },
+  //     { x: moment.utc('2021-05-31T06:00:00.508Z').format('YYYY/MM/DD'), y: 0 },
+  //     { x: moment.utc('2021-06-07T06:00:00.508Z').format('YYYY/MM/DD'), y: 0 },
+  //   ],
+  //   month: [
+  //     { x: moment.utc('2021-01-01T00:00:00.508Z').format('YYYY/MM'), y: 0 },
+  //     { x: moment.utc('2021-02-01T00:00:00.508Z').format('YYYY/MM'), y: 0 },
+  //     { x: moment.utc('2021-03-01T00:00:00.508Z').format('YYYY/MM'), y: 0 },
+  //     { x: moment.utc('2021-04-01T00:00:00.508Z').format('YYYY/MM'), y: 0 },
+  //     { x: moment.utc('2021-05-01T00:00:00.508Z').format('YYYY/MM'), y: 0 },
+  //     { x: moment.utc('2021-06-01T00:00:00.508Z').format('YYYY/MM'), y: 0 },
+  //     { x: moment.utc('2021-07-01T00:00:00.508Z').format('YYYY/MM'), y: 0 },
+  //   ],
+  //   year: [
+  //     { x: moment.utc('2020-01-01T00:00:00.508Z').format('YYYY'), y: 0 },
+  //     { x: moment.utc('2021-01-01T00:00:00.508Z').format('YYYY'), y: 0 },
+  //     { x: moment.utc('2022-01-01T00:00:00.508Z').format('YYYY'), y: 0 },
+  //     { x: moment.utc('2023-01-01T00:00:00.508Z').format('YYYY'), y: 0 },
+  //     { x: moment.utc('2024-01-01T00:00:00.508Z').format('YYYY'), y: 0 },
+  //     { x: moment.utc('2025-01-01T00:00:00.508Z').format('YYYY'), y: 0 },
+  //     { x: moment.utc('2026-01-01T00:00:00.508Z').format('YYYY'), y: 0 },
+  //   ],
+  // } as IChartData;
 
-  const [chartData, setChartData] = React.useState(chart.day as IChartDataItem[]);
+  // const [chartData, setChartData] = React.useState(chart.day as IChartDataItem[]);
 
-  const [dataChart, setDataChart] = React.useState({
-    labels: [],
-    datasets: [
-      {
-        label: 'BTCMT',
-        tension: 0.3,
-        data: chartData,
-        fill: true,
-        backgroundColor: 'rgba(109, 218, 192,0.1)',
-        borderColor: 'rgb(109, 218, 192)',
-        radius: 5,
-        borderWidth: 1,
-        pointHitRadius: 5,
-      },
-    ],
-  });
+  // const [dataChart, setDataChart] = React.useState({
+  //   labels: [],
+  //   datasets: [
+  //     {
+  //       label: 'BTCMT',
+  //       tension: 0.3,
+  //       data: chartData,
+  //       fill: true,
+  //       backgroundColor: 'rgba(109, 218, 192,0.1)',
+  //       borderColor: 'rgb(109, 218, 192)',
+  //       radius: 5,
+  //       borderWidth: 1,
+  //       pointHitRadius: 5,
+  //     },
+  //   ],
+  // });
 
-  const options = {
-    parsing: {
-      xAxisKey: 'x',
-      yAxisKey: 'y',
-    },
-    scales: {
-      xAxes: [
-        {
-          type: 'time',
-          time: { parser: 'YYYY/MM/DD HH:mm:ss' },
-        },
-      ],
-      yAxes: [
-        {
-          display: true,
-          ticks: {
-            beginAtZero: false,
-          },
-          suggestedMax: 600,
-          suggestedMin: 0,
-        },
-      ],
-    },
-    plugins: {
-      legend: {
-        display: false,
-      },
-    },
-  };
+  // const options = {
+  //   parsing: {
+  //     xAxisKey: 'x',
+  //     yAxisKey: 'y',
+  //   },
+  //   scales: {
+  //     xAxes: [
+  //       {
+  //         type: 'time',
+  //         time: { parser: 'YYYY/MM/DD HH:mm:ss' },
+  //       },
+  //     ],
+  //     yAxes: [
+  //       {
+  //         display: true,
+  //         ticks: {
+  //           beginAtZero: false,
+  //         },
+  //         suggestedMax: 600,
+  //         suggestedMin: 0,
+  //       },
+  //     ],
+  //   },
+  //   plugins: {
+  //     legend: {
+  //       display: false,
+  //     },
+  //   },
+  // };
 
   const getInfo = useCallback(async () => {
     const { address } = store.account;
@@ -208,8 +209,8 @@ const Statistic: React.FC = () => {
           clogData('totalStaked (totalStaked): ', value);
           return {
             key: 'totalStaked',
-            // value: normalizedValue(value),
-            value: '-',
+            value: normalizedValue(value),
+            // value: '-',
           };
         }),
       web3Contract.Token.methods
@@ -219,8 +220,8 @@ const Statistic: React.FC = () => {
           clogData('totalIssued (totalIssued): ', value);
           return {
             key: 'totalIssued',
-            // value: normalizedValue(value),
-            value: '-',
+            value: normalizedValue(value),
+            // value: '-',
           };
         }),
       web3Contract.Staking.methods
@@ -230,8 +231,8 @@ const Statistic: React.FC = () => {
           clogData('allTimeMined (allTimeMined): ', value);
           return {
             key: 'allTimeMined',
-            // value: normalizedValue(value),
-            value: '-',
+            value: normalizedValue(value),
+            // value: '-',
           };
         }),
       await getDailyRewards()
@@ -239,8 +240,8 @@ const Statistic: React.FC = () => {
           clogData('estimateDailyRewardsToday (estimateDailyRewardsToday): ', value);
           return {
             key: 'estimateDailyRewardsToday',
-            // value: normalizedValue(value),
-            value: '-',
+            value: normalizedValue(value),
+            // value: '-',
           };
         })
         .catch((err: any) => clogData('rewardPerTokenWithBoostHBTC error:', err)),
@@ -249,8 +250,8 @@ const Statistic: React.FC = () => {
           clogData('rewardPerTokenWithBoostHBTC (rewardPerTokenWithBoostHBTC): ', value);
           return {
             key: 'rewardPerTokenWithBoostHBTC',
-            // value: normalizedValue(value / (1 * 10 ** 18)),
-            value: '-',
+            value: normalizedValue(value / (1 * 10 ** 18)),
+            // value: '-',
           };
         })
         .catch((err: any) => clogData('rewardPerTokenWithBoostHBTC error:', err)),
@@ -261,8 +262,8 @@ const Statistic: React.FC = () => {
             clogData('rewardPerTokenWithBoostUSD: ', res.data['huobi-btc'].usd);
             return {
               key: 'rewardPerTokenWithBoostUSD',
-              // value: res.data['huobi-btc'].usd,
-              value: '-',
+              value: res.data['huobi-btc'].usd,
+              // value: '-',
             };
           },
           (err) => {
@@ -293,37 +294,37 @@ const Statistic: React.FC = () => {
 
   // Functions ------------------------------------------------
 
-  const changeChart = (data: IChartButton) => {
-    if (chartButton === data.id) return;
+  // const changeChart = (data: IChartButton) => {
+  //   if (chartButton === data.id) return;
 
-    const chartButtomsCopy = chartButtons;
+  //   const chartButtomsCopy = chartButtons;
 
-    chartButtomsCopy.map((btn) => {
-      btn.active = btn.id === data.id;
-      return btn;
-    });
+  //   chartButtomsCopy.map((btn) => {
+  //     btn.active = btn.id === data.id;
+  //     return btn;
+  //   });
 
-    setChartButton(data.id);
-    setChartButtons(chartButtomsCopy);
-    setChartData(chart[data.info]);
+  //   setChartButton(data.id);
+  //   setChartButtons(chartButtomsCopy);
+  //   setChartData(chart[data.info]);
 
-    setDataChart({
-      labels: [],
-      datasets: [
-        {
-          label: 'BTCMT',
-          tension: 0.3,
-          data: chart[data.info],
-          fill: true,
-          backgroundColor: 'rgba(109, 218, 192,0.1)',
-          borderColor: 'rgb(109, 218, 192)',
-          radius: 5,
-          borderWidth: 1,
-          pointHitRadius: 5,
-        },
-      ],
-    });
-  };
+  //   setDataChart({
+  //     labels: [],
+  //     datasets: [
+  //       {
+  //         label: 'BTCMT',
+  //         tension: 0.3,
+  //         data: chart[data.info],
+  //         fill: true,
+  //         backgroundColor: 'rgba(109, 218, 192,0.1)',
+  //         borderColor: 'rgb(109, 218, 192)',
+  //         radius: 5,
+  //         borderWidth: 1,
+  //         pointHitRadius: 5,
+  //       },
+  //     ],
+  //   });
+  // };
 
   // Change amounts ------------------------------------------------
 
@@ -349,7 +350,7 @@ const Statistic: React.FC = () => {
   return (
     <div className="stats">
       <div className="stats-data">
-        <div className="stats-data-chart">
+        {/* <div className="stats-data-chart">
           <div className="stats-data-chart-head">
             <div className="stats-data-chart-head-price">
               <span className="stats-data-chart-head-price-title">
@@ -386,7 +387,7 @@ const Statistic: React.FC = () => {
               // ref={(reference) => (chartReference = reference)}
             />
           </div>
-        </div>
+        </div> */}
         <div className="stats-data-info">
           <div className="stats-data-info-item">
             <span className="stats-data-info-item-title">
@@ -417,7 +418,8 @@ const Statistic: React.FC = () => {
               {t('page.statistic.info.boostFactor')}
             </span>
             <span className="stats-data-info-item-value">
-              {/* {(((+info.totalStaked || 0) * 0.1) / 50000).toFixed(6)} */}-
+              {new BigNumber(+info.totalStaked || 0).multipliedBy(0.1).div(50000).toFixed(6)}
+              {/* {((+info.totalStaked || 0) * 0.1) / 50000).toFixed(6)} */}
             </span>
             <span className="stats-data-info-item-line" />
             <span className="stats-data-info-item-subtitle">HBTC</span>
@@ -435,7 +437,11 @@ const Statistic: React.FC = () => {
               {t('page.statistic.info.rewardPerTokenWithBoost')}
             </span>
             <span className="stats-data-info-item-value">
-              {Number.isNaN(
+              {new BigNumber(info.rewardPerTokenWithBoostHBTC || 0)
+                .multipliedBy(info.rewardPerTokenWithBoostUSD || 0)
+                .toFixed(2)}
+
+              {/* {Number.isNaN(
                 (
                   +info.rewardPerTokenWithBoostHBTC * (+info.rewardPerTokenWithBoostUSD || 0)
                 ).toFixed(2),
@@ -443,7 +449,7 @@ const Statistic: React.FC = () => {
                 ? (
                     +info.rewardPerTokenWithBoostHBTC * (+info.rewardPerTokenWithBoostUSD || 0)
                   ).toFixed(2)
-                : '-'}
+                : '-'} */}
             </span>
             <span className="stats-data-info-item-line" />
             <span className="stats-data-info-item-subtitle">USD</span>
